7db5ee1e527d638288bc1e246abc2b43939f2676
Nishibayashi Takuji <takuji@senchan.jp>
2010-10-21 01:43:10 +0900

    スーパーpreのシンタックスハイライトに対応

--- views/helpers/hatena.php
+++ views/helpers/hatena.php
@@ -2,7 +2,17 @@
 require_once('HatenaSyntax.php');
 class HatenaHelper extends Helper{
 
+    public $config = array(
+        'superprehandler' => 'spre_prettify',
+        'htmlescape' => false,
+    );
+
     public function render($data){
-        return $this->output(HatenaSyntax::render($data));
+        return $this->output(HatenaSyntax::render($data,$this->config));
     }
+}
+
+function spre_prettify($type , Array $lines){
+    $body = implode("\n",$lines);
+    return "<pre class=\"prettyprint lang-$type\">\n$body\n</pre>";
 }
